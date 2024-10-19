2c1f07b6f47928e608b9ca2f91dda09ff94fc6bf
.shadyK <145480157+shadyK5@users.noreply.github.com>
2024-10-19 16:22:04 +0800

    NAV LINKS ADDED FOR THE HELP PAGE CONTENT, CHANGED SOME COLORS, TICKET ORDER INFORMATION IS ADDED

---
+++ frontend/src/Screens/TicketOrder.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import '../Designs/Css/TicketOrder.css'; 
+
+const TicketOrder = () => {
+  return (
+    <div className="container">
+      <div className="help-container">
+        <h1 className="help-title">We’re here to help</h1> 
+        <div className="help-buttons">
+          <Link className="help-button" to="/find-events">Attending an Event</Link>
+          <Link className="help-button" to="/organize-events">Organizing an Event</Link>
+          <Link className="help-button" to="/ticket-order">Ticket Order Information</Link>
+          <Link className="help-button" to="/payment">Managing Payments</Link>
+        </div>
+      </div>
+
+      <div className="ticket-order-container">
+        <div className="ticket-details">
+          <p><strong>1 x Ticket</strong></p>
+          <p>Order Total: Free</p>
+        </div>
+
+        <div className="event-details">
+          <p><strong>Date and Time:</strong></p>
+          <p>Friday, September 35, 2088 from 7:00 PM to 10:00 PM (United States of Jupiter Time)</p>
+        </div>
+
+        <div className="venue-details">
+          <p><strong>Location:</strong></p>
+          <p>Ascent Park</p>
+          <p>391 Parkville Road</p>
+          <p>Park</p>
+          <p>United States of Jupiter</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TicketOrder;
